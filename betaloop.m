24edcf1405087f060da78832267935930732d10f
ajaykarpur <ajay@ebookglue.com>
2013-12-26 14:27:04 -0700

    Update betaloop.m

--- betaloop.m
+++ betaloop.m
@@ -3,9 +3,10 @@ beta = linspace(1, 100, 300);
 data = zeros(300, 10000);
 
 for i = 1:300
-    [X, Y, Z] = lorenz(28, 10, beta(i));
-    [rows, columns] = size(X');
-    data(i, 1:columns) = X'(1, :);
+    [X Y Z] = lorenz(28, 10, beta(i));
+    X = X';
+    [rows, columns] = size(X);
+    data(i, 1:columns) = X(1, :);
 end;
 
 j = 1;
