b80a5f3fb4555af57ef82b5d3be94463528b5bf8
bwlarsen <Brett.Larsen14@gmail.com>
2014-01-20 10:49:18 -0700

    Final Report

--- betaloop.m
+++ betaloop.m
@@ -1,21 +1,21 @@
 i = 1;
-beta = linspace(1, 100, 300);
+beta = linspace(1, 10, 300);
 data = zeros(300, 10000);
 
 for i = 1:300
-    [X Y Z] = lorenz(28, 10, beta(i));
+    [X, Y, Z] = lorenz(28, 10, beta(1, i));
     X = X';
     [rows, columns] = size(X);
     data(i, 1:columns) = X(1, :);
 end;
 
-j = 1;
-for i = 1:22689
-    small = min(abs(data(:, i)));
-    if small == 0;
-        small2(j, 1) = i;
-        j = j+1;
-    end
-end
+% j = 1;
+% for i = 1:22689
+%     small = min(abs(data(:, i)));
+%     if small == 0;
+%         small2(j, 1) = i;
+%         j = j+1;
+%     end
+% end
 
-min(small2)
+% min(small2)
--- pcolor_plot.m
+++ pcolor_plot.m
@@ -1,13 +1,23 @@
 Z = data';
 W = hamming(10000);
 
+Fs = 100/10000;
+
 for b = 1:300
     prod = Z(1:10000,b).*W;
     FT = abs(fftshift(fft(prod)));
     C(:, b) = 10.*log10(FT);
 end
 
+x = linspace(1, 10, 300);
+y = Fs/2*linspace(-0.5,0.5,10000)
 figure(1)
-pcolor(C)
+pcolor(x, y, C)
 shading flat
 colorbar
+
+xlabel('Beta Value')
+ylabel('FFT of X Vector')
+title('Pcolor plot of the FFT of the X Vector Over Values of Beta')
+
+%print -painters -dpdf -r300 test.png
